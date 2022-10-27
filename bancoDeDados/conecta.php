4cccda1aae07753a85e5a54af87fd4256f1ff58d
lucasbalbino09 <balbino2409@gmail.com>
2022-10-26 22:13:09 -0300

    Atualizando

--- bancoDeDados/conecta.php
+++ bancoDeDados/conecta.php
@@ -1,11 +1,9 @@
 <?php
-//dados para nos conectarmos ao SGBD
-//Espera o tipo de BD/Qual tabela, Qual maquina(a local) e a porta dele
-$dsn= 'mysql:dbname=senac;host=localhost;port=3307';
-$user='root';
-$pass='';
 
-//conectamos ao SGBD
-$bd=new PDO($dsn, $user, $pass);
+//Dados para nos conectarmos ao SGBD
+$dsn = 'mysql:dbname=senac;host=localhost;port=3307';
+$user = 'root';
+$pass = '';
 
-//scacascascasc
+//Conectamos ao SGBD
+$bd = new PDO($dsn, $user, $pass);
--- formulario/recebe.php
+++ formulario/recebe.php
@@ -1,18 +1,22 @@
 <?php
 
+echo "Seja bem-vindo(a), " . $_POST['nomeUsuario'] . '!<br><br>';
 
-echo "<br>Olá ". $_POST['nomeUsuario'];
+echo "Você tem {$_POST['idadeUsuario']} anos, 
+        CPF: {$_POST['cpfUsuario']}<br>";
 
-echo "<br>Você tem {$_POST['idadeUsuario']}anos, CPF:{$_POST['cpfUsuario']}<br>";
-echo "E o periodo escolhido foi o {$_POST['periodoUsuario']}";
+echo "E o período escolhido foi o {$_POST['periodoUsuario']}";
 
-$nome=$_POST['nomeUsuario'];
-$idade=$_POST['idadeUsuario'];
-$cpf=$_POST['cpfUsuario'];
-$periodo=$_POST['periodoUsuario'];
+$nome = $_POST['nomeUsuario'];
+$idade = $_POST['idadeUsuario'];
+$cpf = $_POST['cpfUsuario'];
+$periodo = $_POST['periodoUsuario'];
 
-$arquivo=fopen('alunos.csv','a');//abro o arquivo para append
-fwrite($arquivo, "$nome;$idade;$cpf;$periodo\r\n");
+//$_SERVER possui muitas variáveis de ambiente que podem ser 
+//muito importantes para você em algum momento, ex: IP do 
+//requisitante
+
+$arquivo = fopen('alunos.csv', 'a');//abro o arquivo para append
+fwrite($arquivo, "$nome;$idade;$cpf;$periodo;{$_SERVER['REMOTE_ADDR']}\r\n");
 fclose($arquivo);
-?>
-//asncoashcioasbnkasc
+
